765e6342c282d03040a274fa6de1c252ecc4922b
Nikhil Bokade <nikhilbokade25@gmail.com>
2025-03-10 13:00:04 -0400

    Implement responsive design for About Component

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,8 +1,51 @@
+import { useEffect } from "react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 import "./About.css";
 import aboutImg from "../Assets/aboutImg.png";
 import aboutImg1 from "../Assets/aboutImg1.jpg";
-export default function About(){
-    return(
+
+gsap.registerPlugin(ScrollTrigger);
+
+export default function About() {
+    useEffect(() => {
+        gsap.fromTo(
+            ".about_text h1",
+            { x: -200, opacity: 0 },
+            {
+                x: 0,
+                opacity: 1,
+                duration: 1, 
+                ease: "power4.out",
+                scrollTrigger: {
+                    trigger: ".about_container",
+                    start: "top 20%", 
+                    end: "top 20%", 
+                    toggleActions: "play none none reverse", 
+                },
+            }
+        );
+
+        gsap.fromTo(
+            ".about_text p",
+            { x: -200, opacity: 0 },
+            {
+                x: 0,
+                opacity: 1,
+                duration: 1, 
+                ease: "power4.out",
+                delay: 0.5, 
+                scrollTrigger: {
+                    trigger: ".about_container",
+                    start: "top 20%", 
+                    end: "top 20%", 
+                    toggleActions: "play none none reverse",
+                },
+            }
+        );
+    }, []);
+
+    return (
         <div className="about_container">
             <div className="about_text">
                 <h1>A New Kind of Sweet Indulgence</h1>
@@ -13,8 +56,8 @@ export default function About(){
 
             <div className="about_image">
                 <div className="about_img">
-                    <img src={aboutImg1}
-/>                </div>
+                    <img src={aboutImg1} alt="ice cream" />
+                </div>
             </div>
 
             <div className="about_overlay">
