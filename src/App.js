ce604325e7e728fa3c30f2d7f434ea725eac7cd2
Nikhil Bokade <nikhilbokade25@gmail.com>
2025-03-10 11:21:29 -0400

    Add Shop Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Hero from './Components/Hero';
+import Shop from './Components/Shop';
 
 function App() {
   return (
     <div className="App">
       <Hero/>
+      <Shop/>
     </div>
   );
 }
---
+++ src/Components/Shop.jsx
@@ -0,0 +1,31 @@
+import './Shop.css';
+import productImg1 from "../Assets/product1.jpg";
+import productImg2 from "../Assets/product2.jpg";
+import productImg3 from "../Assets/product3.jpg";
+import productImg4 from "../Assets/product4.jpg";
+
+export default function Shop() {
+    return (
+        <div className="shop_container">
+            <h1>Scoop in Some Sweetness</h1>
+            <div className='shop_products'>
+                
+                <div className='products'>
+                    <img src={productImg1} alt='product 1' />
+                </div>
+
+                <div className='products'>
+                <img src={productImg2} alt='product 1' />
+                </div>
+
+                <div className='products'>
+                <img src={productImg3} alt='product 1' />
+                </div>
+
+                <div className='products'>
+                <img src={productImg4} alt='product 1' />
+                </div>
+            </div>
+        </div>
+    );
+}
